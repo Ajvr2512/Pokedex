75628b1252d1873a2b09e43880c02d99e1792a3b
Angela Villa <ajvr2512@gmail.com>
2023-04-13 14:36:18 -0500

    Update Home.jsx

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,21 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useContext } from 'react';
 
 const Home = () => {
+  const [nameValue, setNameValue] = useState('');
+  const [nameError, setNameError] = useState(null);
+  const [user, setUser] = useState(useContext);
+
+  const handleChange = (e) => {
+    const newNameValue = e.target.value;
+    setNameValue(newNameValue);
+    if (newNameValue === '') setNameError('Name is required');
+    else if (!/[a-z ]$/i.test(newNameValue))
+      setNameError('Only letters and blanks are allowed');
+    else setNameError(null);
+  };
+  const hanleSubmit = (e) => {
+    e.preventDefault();
+    if (!nameError) setUser(nameValue);
+  };
+
   return (
-    <div>
-      <div>
+    <div className="m-20">
+      <div className="flex justify-center items-center mg-15">
         <img src="/image1.png" alt="Pokedex" />
       </div>
-      <div>
+      <div className="text-center m-10">
         <h1 className="text-red-500 text-4xl font-bold">¡Hello Trainer!</h1>
         <p>Type your name to start</p>
       </div>
-      <form className="flex flex-row justify-center items-center mt-8 gap-3">
-        <input type="text" className="shadow-md border border-black p-3" />
+      <form
+        className="flex flex-row justify-center items-center mt-8 gap-3"
+        onSubmit={hanleSubmit}
+      >
+        <input
+          type="text"
+          value={nameValue}
+          placeholder="Type your name"
+          className="shadow-md border border-black p-3"
+          onChange={handleChange}
+        />
         <button type="submit" className="bg-red-500 text-white font-bold p-3">
           Start
         </button>
       </form>
+      {nameError && <p className="text-red-500 text-center">{nameError}</p>}
     </div>
   );
 };
