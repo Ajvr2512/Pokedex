295261e3ae83fb139f45c4279d5422a32d6feeb5
Angela Villa <ajvr2512@gmail.com>
2023-04-13 14:36:00 -0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,12 @@ import { RouterProvider } from 'react-router';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import { router } from './router';
+import { UserProvider } from './contexts/UserContext';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <UserProvider>
+     <RouterProvider router={router} />
+    </UserProvider>
   </React.StrictMode>,
 );
