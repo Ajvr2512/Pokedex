0d83543311d8ddc82a14c64eaa68e62f090babc2
Angela Villa <ajvr2512@gmail.com>
2023-04-16 02:00:16 -0500

    Update getAllPokemon.js

--- src/services/getAllPokemon.js
+++ src/services/getAllPokemon.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const getAllPokemos = async () => {
   try {
-    const res = await axios.get('https://pokeapi.co/api/v2/pokemon/?limit=300');
+    const res = await axios.get('https://pokeapi.co/api/v2/pokemon/?limit=1300');
     return res.data.results;
   } catch (error) {
     console.error(error);
