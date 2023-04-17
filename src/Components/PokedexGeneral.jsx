ac0759afe1cf9cb45f636f62a541369e50de4b04
Angela Villa <ajvr2512@gmail.com>
2023-04-17 13:13:07 -0500

    Update PokedexGeneral.jsx

--- src/Components/PokedexGeneral.jsx
+++ src/Components/PokedexGeneral.jsx
@@ -1,10 +1,16 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Outlet } from 'react-router';
+import { UserContext } from '../contexts/UserContext';
 
 const PokedexGeneral = () => {
+  const { setUser } = useContext(UserContext);
+  const LogOut = () => {
+    setUser('');
+  };
   return (
     <div>
-      <img src="/image2.png" alt="imgGneral" className="Img_general"/>
+      <img src="/image2.png" alt="imgGneral" className="Img_general" />
+      <button type='text' onClick={LogOut} className="bg-red-500 text-white p-2 hover:bg-red-400 rounded">Log Out</button>
       <div>
         <Outlet />
       </div>
